07c6ce791a37fb72ecc2e808d27a90272ad586fb
denisstuenko <stuenkden@gmail.com>
2024-12-16 16:38:49 +0200

    add cookies functions

--- script.js
+++ script.js
@@ -3,26 +3,51 @@
 // - це нема,
 // +- це може бути,
 // 0 це не встановлено
-document.cookie =
-  "schedule=+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[+,-,+-,0,+,-,+-,0,+,-,+-,0[;max-age=1000000000";
-  let cookies = document.cookie.split(";")
-  console.log (cookies)
 // розділяємо кукі по ; отримуємо дні,
 // потім кожен день ділимо по ,
-let scheduleCookies = ""
-console.log (cookies)
-for(let i = 0;i < cookies.length;i++){
-  if(cookies[i].split("=")[0] == "schedule"){
-    console.log(cookies[i].split("=")[1])
-    scheduleCookies = cookies[i].split("=")[1]
-    break
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
   }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name){
+  const cookies = document.cookie.split("; ")
+  let cookieValue;
+  cookies.forEach(_cookie => {
+    const [_name,value] = _cookie.split("=");
+    if(name === _name) cookieValue = value;
+  })
+  return cookieValue;
 }
 
-const schedule = scheduleCookies
+function convertCookie(){
+  let schedule = [];
+  const scheduleCookie = getCookie("schedule")
+  if(!scheduleCookie){
+    for(let i = 0;i < 7;i++){
+      schedule.push([]);
+      for(let j = 0;j < 24;j++){
+        schedule[i].push("0,")
+      }
+      schedule[i].push("[")
+    }
+    printSchedule();
+    return schedule;
+  }
+  schedule = getCookie("schedule")
   .split("[")
   .map((day) => day.split(",").filter((day) => day != ""))
   .filter((day) => day.length != 0);
+
+  return schedule;
+}
+
+const schedule = convertCookie();
+console.log("schedu",schedule)
 
 console.log("schedule", schedule);
 let select = document.querySelector(".select-day")
@@ -41,19 +66,24 @@ function printSchedule(){
     for(let j = 1; j < hours[i].length; j++){
         if(schedule[i][j] === undefined) {
           hours[i][j].innerHTML = "-"
+          hours[i][j].style.backgroundImage = "none"
+          hours[i][j].style.backgroundColor = "none"
         } else if(schedule[i][j] === "-"){
+          hours[i][j].innerHTML = ""
           hours[i][j].style.backgroundImage = "url(https://www.dtek-oem.com.ua/media/page/no-electricity.png)"
           hours[i][j].style.backgroundColor = " rgba(204, 206, 207, .33)"
         } 
         if(schedule[i][j] === "+") {
+          hours[i][j].innerHTML = ""
           hours[i][j].style.backgroundColor = "yellow"
           hours[i][j].style.backgroundImage = "url(https://media.istockphoto.com/id/1415431745/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B8%D0%BA%D0%BE%D0%BD%D0%B0-%D1%8D%D0%BD%D0%B5%D1%80%D0%B3%D0%B5%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B9-%D0%BC%D0%BE%D0%BB%D0%BD%D0%B8%D0%B8-%D0%BC%D0%BE%D0%BB%D0%BD%D0%B8%D1%8F-%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D1%8D%D0%BD%D0%B5%D1%80%D0%B3%D0%B8%D1%8F-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F-%D0%B8%D0%BB%D0%BB%D1%8E%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F.jpg?s=612x612&w=0&k=20&c=OIqzhGi_kQP1l_mQVCkeuY54iQ8Y34ngJJITDTlHE00=)"
-
         }
         if(schedule[i][j] === "+-") {
+          hours[i][j].innerHTML = ""
           hours[i][j].style.backgroundImage = "url(https://www.dtek-oem.com.ua/media/page/maybe-electricity.png)"
         }
         if(schedule[i][j] === "0") {
+          hours[i][j].innerHTML = ""
           hours[i][j].innerHTML = "-"
         }
     }
@@ -74,8 +104,7 @@ function saveToCookie(){
     }
     scheduleCookies += "["
   }
-  document.cookie = `schedule=${scheduleCookies};max-age=1000000000`;
-  console.log("asdfsa",scheduleCookies)
+  setCookie("schedule",scheduleCookies,365)
 }
 function onAdd(dayNumber, hour, value) {
   schedule[dayNumber][hour] = value;
@@ -85,6 +114,11 @@ function onAdd(dayNumber, hour, value) {
 let dayNameToNumber = {
   "Понеділок":0,
   "Вівторок":1,
+  "Середа":2,
+  "Четвер":3,
+  "П`ятниця":4,
+  "Субота":5,
+  "Неділя":6,
 }
 
 let electricityValues = {
